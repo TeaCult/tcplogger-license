6c05ef779455ec64e6fd8d15297232863fa70198
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-17 04:10:48 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -1,4 +1,5 @@
-pacman-key --init 
+/usr/bin/pacman-key --init
+/usr/bin/pacman-key --populate archlinux
 pacman -Sy --noconfirm stress lm_sensors
 sensors-detect --auto
 stress -c 4 -i 4  --timeout 60 
