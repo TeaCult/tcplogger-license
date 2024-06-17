104a8df872c14562c4db76e648b36c816e644d06
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-17 12:55:01 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -3,5 +3,5 @@
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget
 sensors-detect --auto
 wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/stress.py
-python stress.py 1800
+python stress.py 900
 
