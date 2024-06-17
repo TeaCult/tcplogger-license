96a0bbe4786f976028307328275566d71bb8b05a
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-17 11:06:01 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -2,6 +2,6 @@
 /usr/bin/pacman-key --populate archlinux
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget
 sensors-detect --auto
-stress -c 4 -i 4  --timeout 60 
-sensors 
 wget https://github.com/TeaCult/tcplogger-license/edit/master/stress.py
+python stress.py 60
+sensors
