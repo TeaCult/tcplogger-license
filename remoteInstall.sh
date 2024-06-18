3181d2c969414325560d044ed360889813dabf37
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-18 17:29:30 +0300

    Update remoteInstall.sh

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -1,5 +1,6 @@
 /usr/bin/pacman-key --init
 /usr/bin/pacman-key --populate archlinux
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget nbd qemu-img
+modprobe nbd
 nbd 192.168.5.26 10809 /dev/nbd0 -N myimage
 qemu-img convert -p -f qcow2 -O raw /dev/nbd0 /dev/sda
