593e5988a890e798189f7fabed796cc30498142b
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-19 12:13:18 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -3,7 +3,7 @@
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget
 sensors-detect --auto
 wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/stress.py
-python stress.py 10
+python stress.py 240
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"TestCompleted\"}" http://192.168.5.26:5000/data
 
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)-sensors\": \"$(sensors | tr -d '\n')\"}" http://192.168.5.26:5000/data
