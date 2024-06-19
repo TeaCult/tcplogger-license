d440058531604be93920e25f53ce4af9b37bf869
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-19 12:54:32 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -11,7 +11,7 @@ python stress.py 5
 # badblocks -nsv /dev/sda > badblocks.out
 # curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)-badblocks\": \"$(cat badblocks.out | tr -d '\n')\"}" http://192.168.5.26:5000/data
 
-sudo smartctl -t short /dev/sda
+smartctl -t short /dev/sda
 sleep 120
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)-smartctl\": \"$(smartctl -a /dev/sda | tr -d '\n')\"}" http://192.168.5.26:5000/data
 
