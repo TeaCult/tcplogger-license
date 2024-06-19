94baa3a30cb238f416a27e2e69b38ba41472c0c7
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-19 12:05:41 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -3,9 +3,10 @@
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget
 sensors-detect --auto
 wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/stress.py
-python stress.py 120
-curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp10s0/address)\": \"TestCompleted\"}" http://192.168.5.26:5000/data
-curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp10s0/address)\": \"$(sensors | tr -d '\n')\"}" http://192.168.5.26:5000/data
-badblocks -sv 
-
-
+python stress.py 10
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"TestCompleted\"}" http://192.168.5.26:5000/data
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"$(sensors | tr -d '\n')\"}" http://192.168.5.26:5000/data
+badblocks -nsv /dev/sda
+sudo smartctl -t short /dev/sda
+sleep(120)
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"$(smartctl -a /dev/sda\"}" http://192.168.5.26:5000/data
