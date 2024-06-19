cd61b281ffc534a2c25288dfcd2fe0c2453dc145
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-19 12:52:17 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -4,9 +4,10 @@ pacman -Sy --noconfirm stress lm_sensors dmidecode wget
 sensors-detect --auto
 wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/stress.py
 python stress.py 240
-curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"TestCompleted\"}" http://192.168.5.26:5000/data
 
-curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)-sensors\": \"$(sensors | tr -d '\n')\"}" http://192.168.5.26:5000/data
+# these are built in to stress test 
+# curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"TestCompleted\"}" http://192.168.5.26:5000/data
+# curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)-sensors\": \"$(sensors | tr -d '\n')\"}" http://192.168.5.26:5000/data
 # badblocks -nsv /dev/sda > badblocks.out
 # curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)-badblocks\": \"$(cat badblocks.out | tr -d '\n')\"}" http://192.168.5.26:5000/data
 
