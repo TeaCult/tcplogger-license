b80833f19e531ea353807938bde2f10ab08f28b5
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-20 23:18:44 +0300

    Update remoteInstall.sh

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -3,4 +3,6 @@
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget nbd qemu-img
 modprobe nbd
 nbd-client 192.168.5.26 10809 /dev/nbd0 -N myimage
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Installing tcplogger image\"}" http://192.168.5.26:5000/data
 qemu-img convert -p -f qcow2 -O raw /dev/nbd0 /dev/sda
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Installation of tcplogger image is finished\"}" http://192.168.5.26:5000/data
