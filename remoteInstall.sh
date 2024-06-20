9c5547271670471a9fcff0e8439c2909c71abd67
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-21 01:25:09 +0300

    Update remoteInstall.sh

--- remoteInstall.sh
+++ remoteInstall.sh
@@ -6,3 +6,6 @@ nbd-client 192.168.5.26 10809 /dev/nbd0 -N myimage
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Installing tcplogger image\"}" http://192.168.5.26:5000/data
 qemu-img convert -p -f qcow2 -O raw /dev/nbd0 /dev/sda
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Installation of tcplogger image is finished\"}" http://192.168.5.26:5000/data
+parted /dev/sda resizepart 3 320G 
+e2fsck -f /dev/sda3
+e2fsck -f /dev/sda3 290G
