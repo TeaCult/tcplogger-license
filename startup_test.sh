07276bf2474cebfe614aa43562b3b8196726fa74
Gediz GÜRSU <gedizgursu@gmail.com>
2024-06-22 14:36:23 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -3,19 +3,12 @@
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Installing test packages\"}" http://192.168.5.26:5000/data
 pacman -Sy --noconfirm stress lm_sensors dmidecode wget net-tools
 pacman -Scc --noconfirm
-pacman -Sy --noconfirm python-crypto python-qrcode 
-pacman -Scc --noconfirm 
-pacman -Sy --noconfirm lshw
 sensors-detect --auto
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Finished installing test packages\"}" http://192.168.5.26:5000/data
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Downloading scripts\"}" http://192.168.5.26:5000/data
 wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/stress.py
 wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/checkids.py
-wget https://raw.githubusercontent.com/TeaCult/tcplogger-license/master/qrcode-gen.py
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Finished downloading scripts\"}" http://192.168.5.26:5000/data
-curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Generating qr-code\"}" http://192.168.5.26:5000/data
-python3 qrcode-gen.py $(cat /sys/class/net/enp0s25/address)
-curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Generated qr-code\"}" http://192.168.5.26:5000/data
 
 curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/enp0s25/address)\": \"Started Stress test\"}" http://192.168.5.26:5000/data
 python stress.py 600
