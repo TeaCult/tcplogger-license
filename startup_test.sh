1956266456877ce60e4cc260763faf6c1a517551
Gediz GÜRSU <gedizgursu@gmail.com>
2024-08-25 13:13:43 +0300

    Update startup_test.sh

--- startup_test.sh
+++ startup_test.sh
@@ -35,6 +35,63 @@ else
 fi
 curl -X POST -H "Content-Type: application/json" -d "{\"macadress\": \"All tests are finished nothing more to perform\"}" http://192.168.5.26:5000/data
 
+############ INSTALL PART (remoteinstall.sh) ##############
+
+
+
+# Define variables
+nbd_server="192.168.5.26"
+nbd_base_port=10809
+nbd_device="/dev/nbd0"
+disk_device="/dev/sda"
+max_port_attempts=20
+timeout_duration=30  # Timeout for each NBD connection attempt in seconds
+
+sleep 2
+pacman -Sy --noconfirm qemu-img || exit 1
+sleep 2
+echo "Cleaning cache"
+pacman -Scc --noconfirm
+sleep 2
+echo "loading nbd drivers"
+modprobe nbd || exit 1
+sleep 2
+nbd-client 192.168.5.26 10809 /dev/nbd0 -N myimage || exit 1
+sleep 2 
+# Report installation status to the server
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/$ethname/address)\": \"Installing tcplogger image\"}" http://$nbd_server:5000/data
+sleep 2
+
+# Install the image to the disk
+echo "Installing tcploggerv2 image to the disk"
+qemu-img convert -p -f qcow2 -O raw $nbd_device $disk_device || exit 1
+sleep 2
+
+# Report completion status to the server
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/$ethname/address)\": \"Installation of tcplogger image is finished\"}" http://$nbd_server:5000/data
+sleep 2
+
+# Increase disk size
+echo "Increasing Disk Size"
+parted $disk_device resizepart 3 320G || exit 1
+sleep 2
+e2fsck -f -p ${disk_device}3 || exit 1
+sleep 2
+resize2fs ${disk_device}3 275G || exit 1
+sleep 2
+
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/$ethname/address)\": \"Resizing of disk is finished\"}" http://$nbd_server:5000/data
+sleep 2
+
+# Disconnect from NBD server
+echo "Disconnecting from NBD server"
+nbd-client -d $nbd_device || exit 1
+sleep 2
+
+echo "Installation is Finished you can reboot"
+
+curl -X POST -H "Content-Type: application/json" -d "{\"$(cat /sys/class/net/$ethname/address)\": \"Installation of tcplogger is finished\"}" http://$nbd_server:5000/data
+sleep 2
 
 
 
